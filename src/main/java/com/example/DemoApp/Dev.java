83c8d503e864ddd3f55ff3028cab757429bfaeb0
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-12 21:12:25 +0530

    this is the example of AutoWired field injection

--- src/main/java/com/example/DemoApp/Dev.java
+++ src/main/java/com/example/DemoApp/Dev.java
@@ -1,10 +1,17 @@
 package com.example.DemoApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
+    @Autowired
+    private Laptop laptop;
+//    public Dev(Laptop laptop){
+//        this.laptop=laptop;
+//    }
     public void build(){
         System.out.println("working on Awesome Projects");
+        laptop.compile();
     }
 }
