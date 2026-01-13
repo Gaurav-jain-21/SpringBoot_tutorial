83c5f80f3bed70d47d2d00ab8db45f9e05aab267
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-14 01:27:27 +0530

    this is my first runing

--- First-spring/src/main/java/com/example/demo/Main.java
+++ First-spring/src/main/java/com/example/demo/Main.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context= new ClassPathXmlApplicationContext("applicationBeanContext.xml");
+        ApplicationContext context= new AnnotationConfigApplicationContext(AppConfig.class);
         GreetingService greetingService=(GreetingService) context.getBean("myBean");
         greetingService.sayHello();
 
