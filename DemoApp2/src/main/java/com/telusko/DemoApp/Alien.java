c99c4393d98a66d7f00d088317884be2305b74d7
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-14 23:40:23 +0530

    this is my field injection , constructor, and setter

--- DemoApp2/src/main/java/com/telusko/DemoApp/Alien.java
+++ DemoApp2/src/main/java/com/telusko/DemoApp/Alien.java
@@ -1,10 +1,22 @@
 package com.telusko.DemoApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
+    @Autowired //Field Injection
+    private Laptop laptop;
+//    public Alien(Laptop laptop){
+//        this.laptop= laptop;
+//    }
+//    @Autowired
+//    public void setLaptop(Laptop laptop){
+//        this.laptop= laptop;
+//    }
+
     public void build(){
+        laptop.compile();
         System.out.println("working on Awesome Project");
     }
 }
