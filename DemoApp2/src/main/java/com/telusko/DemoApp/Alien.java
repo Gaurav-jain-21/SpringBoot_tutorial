de9a612c0719e61084ef09bee171a2ca8ed8646d
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-14 22:21:07 +0530

    this is my first Spring boot project on component annotation

---
+++ DemoApp2/src/main/java/com/telusko/DemoApp/Alien.java
@@ -0,0 +1,10 @@
+package com.telusko.DemoApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+    public void build(){
+        System.out.println("working on Awesome Project");
+    }
+}
