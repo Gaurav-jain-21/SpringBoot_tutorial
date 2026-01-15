df1dd07ad1890e6d1fe4e9269315c605aa4ea993
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-15 23:16:31 +0530

    this is the file of controller annotation, restController annotation and requestMapping plus responsebody annotation

---
+++ FirstClass/src/main/java/com/firstExample/FirstClass/FirstClassApplication.java
@@ -0,0 +1,14 @@
+package com.firstExample.FirstClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstClassApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FirstClassApplication.class, args);
+	}
+
+}
---
+++ FirstClass/src/main/java/com/firstExample/FirstClass/HomeController.java
@@ -0,0 +1,15 @@
+package com.firstExample.FirstClass;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String greet(){
+        System.out.println("i am here");
+        return "welcome to the world of Gaurav";
+    }
+}
